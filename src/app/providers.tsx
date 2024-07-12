07ecfbb28e7ad33cdb3474d4617bbed1a395e263
gorkemdurgun <gorkemdurgunn@gmail.com>
2024-07-13 00:48:18 +0300

    refactor: Update provider structure

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,13 +4,17 @@ import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { Provider as ReduxProvider } from "react-redux";
 
 import { persistor, store } from "@/redux/store";
-import { AppConfigProvider } from "./config";
+
 import { PersistGate } from "redux-persist/integration/react";
 import { ApolloClient, ApolloProvider, InMemoryCache, HttpLink } from "@apollo/client";
 
 import apolloClient from "@/utils/apollo";
 import { QueryClientProvider } from "react-query";
 import { queryClient } from "@/utils/api";
+
+import AppConfigProvider from "@/providers/config_provider";
+import I18Provider from "@/providers/i18_provider";
+import AuthProvider from "@/providers/auth_provider";
 
 export default function Providers({
   children,
@@ -22,7 +26,11 @@ export default function Providers({
       <PersistGate persistor={persistor}>
         <ApolloProvider client={apolloClient}>
           <QueryClientProvider client={queryClient}>
-            <AppConfigProvider>{children}</AppConfigProvider>
+            <AppConfigProvider>
+              <I18Provider>
+                <AuthProvider>{children}</AuthProvider>
+              </I18Provider>
+            </AppConfigProvider>
           </QueryClientProvider>
         </ApolloProvider>
       </PersistGate>
--- src/providers/config_provider.tsx
+++ src/providers/config_provider.tsx
@@ -1,16 +1,10 @@
-import { Button, ConfigProvider, ConfigProviderProps, theme } from "antd";
-import { useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "@/hooks";
-
+import { ConfigProvider, ConfigProviderProps, theme } from "antd";
+import { useAppSelector } from "@/hooks";
+import { customTheme } from "@/styles/theme";
 import componentTranslationsTR from "@/localization/components/tr_TR";
 import componentTranslationsEN from "@/localization/components/en_US";
 
-import { customTheme } from "@/styles/theme";
-
-import AuthProvider from "@/providers/auth_provider";
-import I18Provider from "@/providers/i18_provider";
-
-export const AppConfigProvider = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+const AppConfigProvider = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   const { defaultAlgorithm, darkAlgorithm } = theme;
   const { darkMode } = useAppSelector((state) => state.theme);
   const { language } = useAppSelector((state) => state.lang);
@@ -23,9 +17,9 @@ export const AppConfigProvider = ({ children }: Readonly<{ children: React.React
 
   return (
     <ConfigProvider theme={appTheme} locale={appLocale}>
-      <I18Provider>
-        <AuthProvider>{children}</AuthProvider>
-      </I18Provider>
+      {children}
     </ConfigProvider>
   );
 };
+
+export default AppConfigProvider;
