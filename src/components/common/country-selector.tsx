31e427afc6e8097aafe19a76baa2c4c3056aecd4
gorkemdurgun <gorkemdurgunn@gmail.com>
2024-07-31 21:25:06 +0300

    feat: Add CountrySelector component to common components

---
+++ src/components/common/country-selector.tsx
@@ -0,0 +1,40 @@
+import { countries } from "@/constants/countries";
+import { Select } from "antd";
+import { useState } from "react";
+import { PiFlag as FlagIcon } from "react-icons/pi";
+
+type Props = {
+  value: string;
+  onChange: (value: string) => void;
+};
+
+export const CountrySelector: React.FC<Props> = ({ value, onChange }) => {
+  const [activeValue, setActiveValue] = useState<string>(value);
+
+  return (
+    <Select
+      showSearch
+      optionFilterProp="search"
+      placeholder="Select a country"
+      value={activeValue}
+      onChange={(value) => {
+        setActiveValue(value);
+        onChange(value.split(")")[0].replace("(", ""));
+      }}
+      labelRender={(s) => (
+        <div className="flex items-center space-x-2">
+          <FlagIcon className="text-xl" />
+          <span>{s.value}</span>
+        </div>
+      )}
+      options={countries.map((item, index) => {
+        return {
+          key: index,
+          search: item.name,
+          value: "(" + item.code + ")" + " " + item.iso,
+          label: "(" + item.code + ")" + " " + item.name,
+        };
+      })}
+    />
+  );
+};
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,6 @@
 import { TranslatedText } from "./translated-text";
 import { Navbar } from "./navbar";
 import { UserAvatar } from "./user-avatar";
+import { CountrySelector } from "./country-selector";
 
-export { TranslatedText, Navbar, UserAvatar };
+export { TranslatedText, Navbar, UserAvatar, CountrySelector };
