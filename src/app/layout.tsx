77bba4ba151e5a6a1d2dc978a1b7ad77adf79a4c
gorkemdurgun <gorkemdurgunn@gmail.com>
2024-09-01 20:01:28 +0300

    refactor: removed meta, added http"s" to endpoint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+        {/* <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
