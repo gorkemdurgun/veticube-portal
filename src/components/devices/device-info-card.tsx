91f8666e60174f920f7ff6e63d60a1aaf1483f46
gorkemdurgun <gorkemdurgunn@gmail.com>
2024-12-25 23:08:46 +0300

    feat: add loading state to device info card; implement skeleton loading UI for improved user experience

--- src/components/devices/device-info-card.tsx
+++ src/components/devices/device-info-card.tsx
@@ -1,6 +1,7 @@
-import { PiDeviceTablet as DeviceIcon, PiClockClockwise as CommandHistoryIcon, PiGear as SettingsIcon } from "react-icons/pi";
+import { PiDeviceTabletLight as DeviceIcon, PiClockClockwise as CommandHistoryIcon, PiGear as SettingsIcon } from "react-icons/pi";
 
 import { useQuery } from "@apollo/client";
+import { Skeleton } from "antd";
 
 import { deviceQueries } from "@/services/apollo/query";
 
@@ -13,17 +14,24 @@ type DeviceInfoCardProps = {
 };
 
 const DeviceInfoCard = ({ deviceId, onClickCommandLogs, onClickSettings }: DeviceInfoCardProps) => {
-  const { data: deviceInfoData } = useQuery(deviceQueries.GetIotDeviceById, {
+  const { data: deviceInfoData, loading: deviceInfoLoading } = useQuery(deviceQueries.GetIotDeviceById, {
     variables: {
       id: deviceId,
     },
   });
 
-  console.log(deviceInfoData);
+  if (deviceInfoLoading) {
+    return (
+      <div className="w-full flex items-center gap-2 p-4 bg-white rounded-lg">
+        <Skeleton.Avatar active size="large" shape="circle" />
+        <Skeleton.Input style={{ width: 200 }} active size="large" />
+      </div>
+    );
+  }
 
   return (
     <div className="w-full flex items-center gap-2 p-4 bg-white rounded-lg">
-      <DeviceIcon className="w-12 h-12 text-teal-500" />
+      <DeviceIcon className="w-12 h-12 text-gray-500" />
       <div className="flex flex-col gap-1">
         <h5 className="text-md font-semibold text-gray-800">{deviceInfoData?.device.assignments[0].device_nickname}</h5>
         <span className="text-sm text-gray-600">
@@ -33,10 +41,10 @@ const DeviceInfoCard = ({ deviceId, onClickCommandLogs, onClickSettings }: Devic
       </div>
       <div className="ml-auto">
         <div className="flex gap-3">
-          <CustomButton variant="secondary-faded" icon={CommandHistoryIcon} onClick={onClickCommandLogs}>
+          <CustomButton variant="neutral-faded" icon={CommandHistoryIcon} onClick={onClickCommandLogs}>
             Komut Geçmişi
           </CustomButton>
-          <CustomButton variant="secondary-faded" icon={SettingsIcon} onClick={onClickSettings}>
+          <CustomButton variant="neutral-faded" icon={SettingsIcon} onClick={onClickSettings}>
             Ayarlar
           </CustomButton>
         </div>
